03b697588878e3acf3291e8256cecbaeed6a8831
godjiu <61098837+godjiu@users.noreply.github.com>
2026-08-26 10:30:51 +0800

    Remove scheduled update check from TaskManager
    Removed scheduled update check logic and related method.

--- v2rayN/ServiceLib/Manager/TaskManager.cs
+++ v2rayN/ServiceLib/Manager/TaskManager.cs
@@ -71,17 +71,7 @@ public class TaskManager
             }
 
             //Execute once 24 hour
-            if (numOfExecuted % 1440 == 1)
-            {
-                try
-                {
-                    await UpdateTaskRunCheckUpdate();
-                }
-                catch (Exception ex)
-                {
-                    Logging.SaveLog("ScheduledTasks - UpdateTaskRunCheckUpdate", ex);
-                }
-            }
+            
             numOfExecuted++;
         }
     }
@@ -130,22 +120,5 @@ public class TaskManager
         }
     }
 
-    private async Task UpdateTaskRunCheckUpdate()
-    {
-        Logging.SaveLog("Execute check update");
-
-        var updateService = new UpdateService(_config, async (success, msg) => await Task.CompletedTask);
-
-        var msgs = await updateService.CheckHasUpdateOnlyAll(_config.CheckUpdateItem.CheckPreReleaseUpdate);
-        foreach (var msg in msgs)
-        {
-            await _updateFunc?.Invoke(false, msg);
-        }
-        NoticeManager.Instance.Enqueue(string.Join("\n", msgs));
-
-        if (msgs.Count > 0)
-        {
-            AppEvents.HasUpdateNotified.Publish(true);
-        }
-    }
+    
 }
